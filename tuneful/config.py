3169a95847bc3258a45129ce6fa1b85d55b74d82
Joe Turner <joe@oampo.co.uk>
2014-12-15 17:20:04 +0000

    Initial commit

---
+++ tuneful/config.py
@@ -0,0 +1,9 @@
+class DevelopmentConfig(object):
+    DATABASE_URI = "postgresql://action:action@localhost:5432/tuneful"
+    DEBUG = True
+    UPLOAD_FOLDER = "uploads"
+
+class TestingConfig(object):
+    DATABASE_URI = "postgresql://action:action@localhost:5432/tuneful-test"
+    DEBUG = True
+    UPLOAD_FOLDER = "test-uploads"
---
+++ tuneful/models.py
@@ -0,0 +1,9 @@
+import os.path
+
+from flask import url_for
+from sqlalchemy import Column, Integer, String, Sequence, ForeignKey
+from sqlalchemy.orm import relationship
+
+from tuneful import app
+from database import Base, engine
+
